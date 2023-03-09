8f201196ae4d04b4b3d75b42a31eb3b6059231e2
Alejandro Bernabeu <117915926+Aberdur@users.noreply.github.com>
2023-03-09 16:49:09 +0100

    Matrix_to_file_writer

--- diversity_tools/src/matrix_writers.py
+++ diversity_tools/src/matrix_writers.py
@@ -3,4 +3,6 @@ import pandas as pd
 def write_file_from_matrix(families_matrix, fpath):
     df = pd.DataFrame(families_matrix)
     df = df.fillna(value=0)
-    df.to_csv(fpath, float_format='%.2f')
+    df = df.astype(int)
+    df = df.T
+    df.to_csv(fpath, index_label="#ID")
--- diversity_tools/tests/test_writers.py
+++ diversity_tools/tests/test_writers.py
@@ -1,6 +1,7 @@
 import unittest
 import csv
 
+from os import remove as remove_file
 from pathlib import Path
 
 from src.matrix_writers import write_file_from_matrix
@@ -9,6 +10,9 @@ class  TestReaders(unittest.TestCase):
 
     def setUp(self):
         self.test_path = Path(__file__).parent.absolute() / "test_data"
+
+    def tearDown(self):
+        remove_file(self.test_path/ "geneFamiliesMatrix_to_File_input.csv")
     
     def test_write_file_from_matrix(self):
         fpath = self.test_path / "geneFamiliesMatrix_to_File_input.csv"
@@ -21,5 +25,11 @@ class  TestReaders(unittest.TestCase):
                                     "SP4": 2, 
                                     "SP5": 2}}
         write_file_from_matrix(family_matrix, fpath)
-        assert family_matrix["CL0001"]["SP2"] == 3
-        for line in csv.DictReader(self):
+        with open(fpath) as fhand:
+            for line in csv.DictReader(fhand):
+                print(line)
+                assert line == {'#ID':'CL0001', 'SP1': '2', 'SP2': '3', 'SP3': '1', 'SP4': '0', 'SP5': '0'}
+                assert line["#ID"] == "CL0001"
+                assert int(line["SP1"]) == 2
+                assert int(line["SP4"]) == 0
+                break
