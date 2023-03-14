0ec2911c5fd90f590ded990af428c2954edd1246
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2023-03-14 15:09:54 +0100

    Update matrix_operations.py

--- diversity_tools/src/matrix_operations.py
+++ diversity_tools/src/matrix_operations.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def convert_list_to_numbers(gene_families):
     gene_families_counts = {}
     for family_names, species in gene_families.items():
@@ -7,3 +10,9 @@ def convert_list_to_numbers(gene_families):
                 if species_names not in gene_families_counts[family_names]:
                     gene_families_counts[family_names][species_names] = len(gene_names)
     return gene_families_counts
+
+def convert_into_dataframe(gene_families):
+    df = pd.DataFrame(gene_families)
+    df = df.fillna(value=0)
+    df = df.astype(int)
+    return df
