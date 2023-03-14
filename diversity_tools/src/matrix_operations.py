54006f16e2e8a15bd76a388234beaabcf3fc05b8
Alejandro Bernabeu <117915926+Aberdur@users.noreply.github.com>
2023-03-14 15:10:43 +0100

    Update matrix_operation.py

--- diversity_tools/src/matrix_operations.py
+++ diversity_tools/src/matrix_operations.py
@@ -7,3 +7,11 @@ def convert_list_to_numbers(gene_families):
                 if species_names not in gene_families_counts[family_names]:
                     gene_families_counts[family_names][species_names] = len(gene_names)
     return gene_families_counts
+
+
+def calculate_shannon_diversity_index(gene_families):
+    g_sum = df.groupby('Name_Receive')['Amount'].transform('sum')
+    values = df['Amount']/g_sum
+    df['Entropy'] = -(values*np.log(values))
+
+    df1 = df.groupby('Name_Receive',as_index=False,sort=False)['Entropy'].sum()
