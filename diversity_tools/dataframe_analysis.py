1e073f99dc1edc943dd593e9f4ab02a3a77e2963
Alejandro Bernabeu <117915926+Aberdur@users.noreply.github.com>
2023-04-14 12:05:16 +0200

    Readme and Log update

--- diversity_tools/dataframe_analysis.py
+++ diversity_tools/dataframe_analysis.py
@@ -7,8 +7,7 @@ from src.matrix_readers import read_orthovenn2_composition_output
 from src.matrix_operations import (convert_list_to_numbers, convert_into_dataframe, get_dataframe_with_limited_families,
                                    filter_dataframe_cols_by_value_occurrence, select_families_with_highest_number_of_genes,
                                    filter_dataframe_by_cols_name, filter_dataframe_by_rows_name, calculate_shannon_diversity_index)
-from src.matrix_writers import write_csv_from_matrix #, write_file_from_matrix
-from src.plots import convert_diversity_matrix_to_graph
+from src.matrix_writers import write_csv_from_matrix
 
 def parse_arguments():
     desc = "Analysis biological features from OrthoVenn2 (.txt file) DataFrame"
@@ -48,14 +47,18 @@ def main():
     operations = options["operations"]
     out_fdir = Path(options["out_fdir"])
 
-    if not out_fdir.exists():
-        out_fdir.mkdir()
+    main_fdir = Path("C:/Users/aleja/OneDrive - UPV/Documentos/Trabajo_Fin_de_Grado/results_folder")
+    dir_path = main_fdir /  out_fdir
+
+    if not dir_path.exists():
+        dir_path.mkdir()
 
     with open(Path(options["Input_fpath"])) as fhand:
         input_list = read_orthovenn2_composition_output(fhand)
 
     list_to_numbers = convert_list_to_numbers(input_list)
-    df_matrix = convert_into_dataframe(list_to_numbers) 
+    df_matrix = convert_into_dataframe(list_to_numbers)
+    message = ""
 
     for operation in operations:
         operation_command, arguments_to_parse = operation.split("=")
@@ -65,7 +68,9 @@ def main():
             argument, value = arguments_to_parse.split(",")
             arguments[argument] = value
             arguments["df"] = df_matrix
+            message += "\n" + f"The DataFrame has a original length of {df_matrix.shape[1]} columns and"
             df_matrix = get_dataframe_with_limited_families(**arguments)
+            message += f" has been reduced to a length of {df_matrix.shape[1]} columns"
 
         if operation_command == "filter":
             arguments = {}
@@ -73,24 +78,34 @@ def main():
                 argument, value = argument.split(",")
                 arguments[argument] = value
             arguments["df_matrix"] = df_matrix
+            if "value" in arguments:
+                message_value = arguments["value"]
+                message += "\n" + f"The DataFrame has been applied a filter where {message_value} is the value to filter on,"
             if "threshold" in arguments:
                 arguments["threshold"] = float(arguments["threshold"])
+                message_threshold = arguments["threshold"]
+                message += f" with a threshold cutoff of {message_threshold}/1." + "\n"
+            if "mode" in arguments:
+                message_mode = arguments["mode"]
+                message += f"The mode applied is: {message_mode}, which determines whether to retain uppercase (greater_than), lowercase (lower_than), or same (equal) types of values, "
             if "ignore_zeros" in arguments:
                 if arguments["ignore_zeros"] == "False":
                     arguments["ignore_zeros"] = False
+                    message += f"without ingoring 0."
                 elif arguments["ignore_zeros"] == "True":
                     arguments["ignore_zeros"] = True
+                    message += f"ingoring 0."
                 else:
                     raise ValueError("ignore zeros should be True or False")
             df_matrix = filter_dataframe_cols_by_value_occurrence(**arguments)
-
+            
         if operation_command == "Highest_value":
             arguments = {}
             argument, value = arguments_to_parse.split(",")
             arguments[argument] = int(value)
             arguments["df_matrix"] = df_matrix
             df_matrix = select_families_with_highest_number_of_genes(**arguments)
-            print(df_matrix)
+            message += "\n" + f"Has been selected the {value} number of families with the highest number of genes to keep in the DataFrame."
 
         if operation_command == "Cols":
             arguments = {}
@@ -100,14 +115,20 @@ def main():
                     value = value.split(",")
                 arguments[argument] = value
             arguments["df_matrix"] = df_matrix
+            if "column_name_list" in arguments:
+                message_cols_list = arguments["column_name_list"]
+                message += "\n" + f"{message_cols_list} is a list of column names to "
             if "keep_columns" in arguments:
                 if arguments["keep_columns"] == "False":
                     arguments["keep_columns"] = False
+                    message += f"exclude  from DataFrame"
                 elif arguments["keep_columns"] == "True":
                     arguments["keep_columns"] = True
+                    message += f"keep from DataFrame"
                 else:
                     raise ValueError("keep_columns should be True or False")
             df_matrix = filter_dataframe_by_cols_name(**arguments)
+            
 
         if operation_command == "Rows":
             arguments = {}
@@ -117,29 +138,29 @@ def main():
                     value = value.split(",")
                 arguments[argument] = value
             arguments["df_matrix"] = df_matrix
+            if "row_name_list" in arguments:
+                message_list = arguments["row_name_list"]
+                message += "\n" + f"{message_list} is a list of rows names to "
             if "keep_row" in arguments:
                 if arguments["keep_row"] == "False":
                     arguments["keep_row"] = False
+                    message += f"exclude from DataFrame"
                 elif arguments["keep_row"] == "True":
                     arguments["keep_row"] = True
+                    message += f"keep from DataFrame"
                 else:
                     raise ValueError("keep_row should be True or False")
             df_matrix = filter_dataframe_by_rows_name(**arguments)
     
-    out_fpath = out_fdir / "Analyzed_DataFrame.csv"
+    out_fpath = dir_path / "Analyzed_DataFrame.csv"
     with open(out_fpath, "w") as out_fhand:
         write_csv_from_matrix(df_matrix, out_fhand)
-    
-    out_plot_fpath = out_fdir / "Families_Diversity_plot.svg"
-    df_to_plot = calculate_shannon_diversity_index(df_matrix)
-    convert_diversity_matrix_to_graph(df_to_plot, out_plot_fpath)
 
-    run_log_fpath = out_fdir / "run.log.txt"
+    run_log_fpath = dir_path / "run.log.txt"
     with open(run_log_fpath, "w") as log_fhand:
-        log_fhand.write("\t".join(argv)) 
+        log_fhand.write("\t".join(argv))
+        log_fhand.write(message)
 
-    #Añadir al Log una explicación de las operaciones que se ha hecho sobre el data frame
-    
 
 if __name__ == "__main__":
     main()
--- diversity_tools/generate_diversity_plot.py
+++ diversity_tools/generate_diversity_plot.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from src.matrix_readers import read_orthovenn2_composition_output, read_matrix_from_file
 from src.matrix_operations import convert_list_to_numbers, convert_into_dataframe, calculate_shannon_diversity_index
-from src.matrix_writers import write_file_from_matrix, write_csv_from_matrix
+from src.matrix_writers import write_csv_from_matrix
 from src.plots import convert_diversity_matrix_to_graph
 
 def parse_arguments():
@@ -44,8 +44,12 @@ def main():
     options = get_options()
     format = options["fileFormat"]
     out_fdir = options["out_fdir"]
-    if not out_fdir.exists():
-        out_fdir.mkdir()
+
+    main_fdir = Path("C:/Users/aleja/OneDrive - UPV/Documentos/Trabajo_Fin_de_Grado/results_folder")
+    dir_path = main_fdir /  out_fdir
+
+    if not dir_path.exists():
+        dir_path.mkdir()
 
     if format == "ov":
         with open(Path(options["Input_fpath"])) as fhand:
@@ -57,14 +61,13 @@ def main():
             List_to_numbers = read_matrix_from_file(fhand, family_field="#ID")
 
     df_matrix = convert_into_dataframe(List_to_numbers)
-    print(df_matrix)
+    df_matrix = df_matrix.T
     Diversity_df = calculate_shannon_diversity_index(df_matrix)
-    print(Diversity_df)
 
-    out_plot_fpath = out_fdir / "Diversity_DataFrame.svg"
+    out_plot_fpath = dir_path / "Diversity_DataFrame.svg"
     convert_diversity_matrix_to_graph(Diversity_df, out_plot_fpath)
 
-    out_fpath = out_fdir / "Diversity_DataFrame.csv"
+    out_fpath = dir_path / "Diversity_DataFrame.csv"
     with open(out_fpath, "w") as out_fhand:
         write_csv_from_matrix(Diversity_df, out_fhand)
 
