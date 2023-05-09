f0d2fb6be870ca3788e17133fe5b752dcf5cdf47
Alejandro Bernabeu <117915926+Aberdur@users.noreply.github.com>
2023-05-09 10:45:23 +0200

    Plot_generator_Update
    DIversity and Specialization plot_generator works correctly.
    I've started a new function that generates a scatter plot from diversity and specialization results

--- diversity_tools/plot_generator.py
+++ diversity_tools/plot_generator.py
@@ -1,10 +1,11 @@
 import argparse
 
 from pathlib import Path
+from sys import argv
 
 from src.matrix_readers import read_orthovenn2_composition_output, read_matrix_from_file
 from src.matrix_operations import convert_list_to_numbers, convert_into_dataframe, calculate_shannon_diversity_index
-from src.plots import convert_diversity_matrix_to_graph, convert_specialization_matrix_to_graph
+from src.plots import convert_diversity_matrix_to_graph, convert_specialization_matrix_to_graph, convert_dataframe_to_scatter
 
 def parse_arguments():
     desc = "Extract biological features from 'ov' -OrthoVenn2- or 'csv' file and generate diversity plot"
@@ -51,9 +52,20 @@ def main():
         dir_path.mkdir()
 
     with open(Path(options["Input_fpath"])) as fhand:
-            dataframe = read_matrix_from_file(fhand, family_field="#ID")
-            df_matrix = convert_into_dataframe(dataframe)
-            df_matrix = df_matrix.T
+            if operations == "scatter":
+                diversity_df, Specialization_df = fhand.split("-")
+                diversity_df = read_matrix_from_file(diversity_df, family_field="#ID")
+                diversity_df = convert_into_dataframe(diversity_df)
+                diversity_df = diversity_df.T
+                Specialization_df = read_matrix_from_file(Specialization_df, family_field="#ID")
+                Specialization_df = convert_into_dataframe(Specialization_df)
+                diversity_df = diversity_df.T
+            else: 
+                dataframe = read_matrix_from_file(fhand, family_field="#ID")
+                df_matrix = convert_into_dataframe(dataframe)
+                df_matrix = df_matrix.T
+
+    message = ""
 
     for operation in operations:
         if operation == "diversity":
@@ -62,10 +74,20 @@ def main():
             message += "\n" + f"Has been applied the shannon diversity index to the dataframe"
 
         if operation == "specialization":
-            out_plot_fpath = dir_path / "Specificity_Specialization_Analyzed_DataFrameDataFrame.svg"
+            out_plot_fpath = dir_path / "Specialization_Analyzed_DataFrame.svg"
             convert_specialization_matrix_to_graph(df_matrix, out_plot_fpath)
             message += "\n" + f"Has been applied the shannon diversity index to the dataframe"
 
+        if operation == "scatter":
+            out_plot_fpath = dir_path / "Diversity_Specialization_ScatterPlot.svg"
+            convert_dataframe_to_scatter(diversity_df, Specialization_df, out_plot_fpath)
+            message += "\n" + f"Has been applied the shannon diversity index to the dataframe"
+
+
+    run_log_fpath = dir_path / "run.log.txt"
+    with open(run_log_fpath, "w") as log_fhand:
+        log_fhand.write("\t".join(argv))
+        log_fhand.write(message)
 
 if __name__ == "__main__":
     main()
--- diversity_tools/src/plots.py
+++ diversity_tools/src/plots.py
@@ -1,5 +1,6 @@
 from matplotlib_venn import venn2, venn3
 
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.style as style
@@ -21,18 +22,22 @@ def venn_diagram_of_kmers(grouped_kmers, percentages=True):
         return venn3(sets, set(groups), subset_label_formatter=lambda x: f"{(x/total):1.0%}"), sets, groups
     elif len(groups) == 3:
         return venn3(sets, set(groups)), sets, groups
-    
 
 def convert_diversity_matrix_to_graph(diversity_matrix, out_fpath):
-    diversity_matrix = diversity_matrix.to_frame()
     diversity_matrix["Families"] = diversity_matrix.index
-    diversity_matrix = diversity_matrix.rename(columns={0: "Diversity"})
+    diversity_matrix = diversity_matrix.rename(columns={'0': "Diversity"})
+    print(diversity_matrix)
     sns.barplot(x="Families", y="Diversity", data=diversity_matrix, palette=("Blues_d")).set(title='Diversity of selected Families')
     plt.savefig(out_fpath)
 
 def convert_specialization_matrix_to_graph(specialization_matrix, out_fpath):
-    specialization_matrix = specialization_matrix.to_frame()
     specialization_matrix["Families"] = specialization_matrix.index
-    specialization_matrix = specialization_matrix.rename(columns={0: "Diversity"})
+    specialization_matrix = specialization_matrix.rename(columns={'0': "Specialization"})
     sns.barplot(x="Families", y="Specialization", data=specialization_matrix, palette=("Blues_d")).set(title='Specialization of selected Families')
     plt.savefig(out_fpath)
+
+def convert_dataframe_to_scatter(diversity_matrix, specialization_matriz, out_fpath):
+    diversity_matrix = diversity_matrix.rename(columns={'0': "Diversity"})
+    specialization_matrix = specialization_matrix.rename(columns={'0': "Specialization"})
+    scatter_matrix = pd.concat([diversity_matrix, specialization_matriz], axis = 1)
+    sns.jointplot(x = scatter_matrix['Diversity'], y = scatter_matrix['Specialization'], palette=("Blues_d"), kind = 'scatter')
