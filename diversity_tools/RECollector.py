32ec386cda82134473654e1a0c4c82d0459185dc
AgustinAmata <137272008+AgustinAmata@users.noreply.github.com>
2023-10-11 12:30:31 +0200

    Update RECollector.py

--- diversity_tools/RECollector.py
+++ diversity_tools/RECollector.py
@@ -1,15 +1,16 @@
 import argparse
+import gc
+import sys
 from pathlib import Path
 
-
-from src.matrix_operations import (create_te_count_matrix,
+from src.create_matrix import (create_te_count_matrix,
                                count_tes, filter_df_by_chromosomes,
                                filter_df_by_domain,
                                filter_df_by_length,
                                filter_df_by_percentages) 
-from src.matrix_readers import (merge_inputs, read_repeatmasker_out,
+from src.read_input import (merge_inputs, read_repeatmasker_out,
                             read_tesorter_cls_tsv)
-from src.general_utils import (convert_data_to_long_df_div,
+from src.utils import (convert_data_to_long_df_div,
                        read_chroms_file, read_doms_file,
                        read_names_file)
 
@@ -17,9 +18,10 @@ from src.general_utils import (convert_data_to_long_df_div,
 def argument_parser():
     desc = """Create a TE count matrix and a divergence table 
     from several files of RepeatMasker (RM) and TESorter (TES);
-    TESorter files must come from RM. Several options are provided
-    to filter the data from these files (some of them require input
-    files). Another file with the names of the species is required."""
+    TES files must come from RM individual repeats. Several options
+    are provided to filter the data from these files (some of them
+    require input files). Another file with the names of the species
+    is required."""
     parser = argparse.ArgumentParser(description=desc)
         
     help_input_dir = """Input directory containing the files
@@ -32,7 +34,7 @@ def argument_parser():
     for each RM/TES file. File must contain the common part for
     both RM and TES files, followed by the name of the species 
     (use underscores instead of whitespaces) and separated
-    by a tab. Example:Peame105  Persea_americana"""
+    by a tab. Example: Peame105  Persea_americana"""
     parser.add_argument("--names", "-n", type=Path,
                         help=help_input_names_file, required=True)
 
@@ -98,17 +100,24 @@ def argument_parser():
                             nargs=1)
 
     help_matrix_depth = """Select the depth of the TE count matrix
-    (class, superfamily, tes order, tes superfamily, clade, domains)"""
-    depth_choices = ["superfamily","class", "tes order",
-                     "tes superfamily", "clade", "domains"]
+    (class, superfamily, tes order, tes superfamily, clade).
+    Default superfamily."""
+    depth_choices = ["superfamily", "class", "tes order",
+                     "tes superfamily", "clade"]
     parser.add_argument("--depth", help=help_matrix_depth,
                         choices=depth_choices,
                         default="superfamily", required=False)
-
+    help_override = """When selected, and when selected depth for the TE
+    count matrix is 'superfamily' or 'class', repeats with unknown values
+    for these columns will be overriden by their correspondent
+    'tes superfamily' and 'tes order', respectively (if there are values
+    for these columns in a given repeat)."""
+    parser.add_argument("--override", help=help_override,
+                        action="store_true", required=False)
     help_output = """Output folder path. Generated files will
     be in .csv format"""
     parser.add_argument("--output", "-o", help=help_output,
-                        required=True)
+                        type=Path, required=True)
 
     return parser
 
@@ -118,80 +127,33 @@ def get_options():
 
 def main():
     arguments = get_options()
+    depth = arguments.depth
+    override = arguments.override
 
     root_dir = arguments.input
     rm_dir = root_dir / "RepeatMasker"
     te_dir = root_dir / "TESorter"
-    rm_files = rm_dir.glob("*.out")
-    te_files = te_dir.glob("*.tsv")
+
+    out_folder = arguments.output
+    if not out_folder.exists():
+        out_folder.mkdir()
+    div_folder = out_folder.joinpath(f"{depth}_divergence_files")
+    if not div_folder.exists():
+        div_folder.mkdir()
 
     names_file = arguments.names
     with open(names_file) as names:
         filehand_species = read_names_file(names)
         print("Read names of species file")
 
-    read_inputs = {}
-
-    for rm_file in rm_files:
-        with open(rm_file) as rm_fhand:
-            fhand_repeats = read_repeatmasker_out(rm_fhand)
-            for filehand in filehand_species.keys():
-                if filehand in rm_file.name:
-                    sp_name = filehand_species[filehand]
-                    read_inputs[sp_name] = [fhand_repeats]
-                    break
-            print(f"Read {rm_file.name} -> {sp_name}")
-
-    for te_file in te_files:
-        with open(te_file) as te_fhand:
-            fhand_repeats = read_tesorter_cls_tsv(te_fhand)
-            for filehand in filehand_species.keys():
-                if filehand in te_file.name:
-                    sp_name = filehand_species[filehand]
-                    read_inputs[sp_name].append(fhand_repeats)
-                    break
-            print(f"Read {te_file.name} -> {sp_name}")
-
-    print("Merging inputs")
-    species_dfs = {}
-    for species in read_inputs:
-        rm_input = read_inputs[species][0]
-        te_input = read_inputs[species][1]
-        merged_inp = merge_inputs(rm_input, te_input)
-        species_dfs[species] = merged_inp
-        print(f"Merged inputs from {species}. Dataframe created")
-
-    if arguments.length:
-        print("Started filtering by length")
-        length = arguments.length
-        for species in species_dfs:
-            sp_df = species_dfs[species]
-            filtered_df = filter_df_by_length(sp_df, length)
-            species_dfs.update({species:filtered_df})
-            print(f"Filtered data from {species}")
-
-        print("Finished filtering by length")
-
     if arguments.chrom:
-        print("Started filtering by chromosomes")
-
         chroms_file = arguments.chrom
+        chr_mode = arguments.E
         with open(chroms_file) as chroms:
             chrs_to_filter = read_chroms_file(chroms)
             print("Read file detailing chromosomes to filter")
 
-        for species in chrs_to_filter:
-            sp_df = species_dfs[species]
-            selected_chrs = chrs_to_filter[species]
-            chr_mode = arguments.E
-            filtered_df = filter_df_by_chromosomes(sp_df, selected_chrs, chr_mode)
-            species_dfs.update({species:filtered_df})
-            print(f"Filtered data from {species}")
-
-        print("Finished filtering by chromosomes")
-
     if arguments.domains:
-        print("Started filtering by domains")
         if arguments.D:
             with open(arguments.D) as doms_file:
                 domains, clades, features_dict = read_doms_file(doms_file)
@@ -201,55 +163,106 @@ def main():
             domains = []
             clades = []
             features_dict = []
-
-        for species in species_dfs:
-            sp_df = species_dfs[species]
-            filtered_df = filter_df_by_domain(sp_df, domains, clades, features_dict)
-            species_dfs.update({species:filtered_df})
-            print(f"Filtered data from {species}")
-
-        print("Finished filtering by domains")
+            print("Created conditions for domain filtering")
 
     if arguments.per:
-        print("Started filtering by percentage")
-
         threshold = arguments.t
         perc_mode = arguments.m
-        for species in species_dfs:
-            sp_df = species_dfs[species]
-            filtered_df = filter_df_by_percentages(sp_df, threshold, perc_mode)
-            species_dfs.update({species:filtered_df})
-            print(f"Filtered data from {species}")
-
-        print("Finished filtering by percentage")
+        print("Gathered conditions for percentage filtering")
 
     species_counted_tes = []
-    depth = arguments.depth
-    for species in species_dfs:
-        sp_df = species_dfs[species]
-        counted_tes = count_tes(sp_df, species, depth)
-        species_counted_tes.append(counted_tes)
+    for filehand in filehand_species:
+        species = filehand_species[filehand]
+        print(f"{'-'*10} Collecting data for {species} {'-'*10}")
+        rm_file = list(rm_dir.glob(f"{filehand}*.out"))
+        te_file = list(te_dir.glob(f"{filehand}*.cls.tsv"))
 
+        if len(rm_file) != 1:
+            print("RepeatMasker file was not found/file name in names file was not clear enough")
+            print("Proceding with next species")
+            continue
+
+        if len(te_file) != 1:
+            print("RepeatMasker file was not found/file name in names file was not clear enough")
+            print("Proceding with next species")
+            continue
+
+        with open(rm_file[0]) as rm_fhand:
+            print(f"Reading {rm_file[0].name}")
+            rm_repeats = read_repeatmasker_out(rm_fhand)
+            print(f"Read {rm_file[0].name}")
+        with open(te_file[0]) as te_fhand:
+            print(f"Reading {te_file[0].name}")    
+            te_repeats = read_tesorter_cls_tsv(te_fhand)
+            print(f"Read {te_file[0].name}")
+
+        species_df = merge_inputs(rm_repeats, te_repeats)
+        print("Merged input files into a dataframe")
+
+        del rm_repeats, te_repeats
+
+        if arguments.length:
+            print("Started filtering by length")
+            length = arguments.length
+            species_df = filter_df_by_length(species_df, length)
+            print("Finished filtering by length")
+
+        if arguments.chrom:
+            print("Started filtering by chromosomes")
+            selected_chrs = chrs_to_filter[species]
+            species_df = filter_df_by_chromosomes(species_df,
+                                                  selected_chrs,
+                                                  chr_mode)
+            print("Finished filtering by chromosomes")
+
+        if arguments.domains:
+            print("Started filtering by domains")
+            species_df = filter_df_by_domain(species_df,
+                                             domains, clades,
+                                             features_dict)
+            print("Finished filtering by domains")
+
+        if arguments.per:
+            print("Started filtering by percentage")
+            species_df = filter_df_by_percentages(species_df,
+                                                  threshold,
+                                                  perc_mode)
+            print("Finished filtering by percentage")
+
+        print(f"Counting TEs for {depth}")
+        counted_tes = count_tes(species_df, species, depth, override)
+        species_counted_tes.append(counted_tes)
+        print(f"Counted TEs for {depth}")
+        
+        print(f"{'*'*5} Creating {depth} divergence data file(s) {'*'*5}") 
+        depth_cats = species_df[depth].unique()   
+        for cat in depth_cats:
+            cat_df = species_df.loc[species_df[depth] == cat]
+            long_df_div = convert_data_to_long_df_div(cat_df, species, depth)
+            div_csv_fpath = div_folder.joinpath(f"{cat}_divergence.csv")
+            if not div_csv_fpath.exists():
+                long_df_div.to_csv(div_csv_fpath, index=False,
+                                    chunksize=100000)
+                print(f"{cat.capitalize()} divergence data file created")
+
+            else:
+                long_df_div.to_csv(div_csv_fpath, mode="a",
+                                    index=False, header=False,
+                                    chunksize=100000)
+                print(f"{cat.capitalize()} divergence data file updated")
+
+        del species_df, long_df_div
+        gc.collect()
+    
+    print(f"{'-'*10} Performed operations for all accepted species {'-'*10}")
     print("Creating TE count matrix")
     te_count_matrix = create_te_count_matrix(species_counted_tes)
     print("TE count matrix created")
 
-    print("Creating species divergence data")
-    long_df_div = convert_data_to_long_df_div(species_dfs, depth)
-    print("Species divergence data created")
-
-    out_folder = Path(arguments.output)
-    if not out_folder.exists():
-        out_folder.mkdir()
-
     c_matrix_fpath = out_folder.joinpath(f"{out_folder.name}_count_matrix.csv")
-    div_csv_fpath = out_folder.joinpath(f"{out_folder.name}_divergence.csv")
 
-    te_count_matrix.to_csv(c_matrix_fpath, index_label= depth)
+    te_count_matrix.to_csv(c_matrix_fpath, index_label=depth)
     print("TE count matrix file generated")
-
-    long_df_div.to_csv(div_csv_fpath)
-    print("Species divergence data file generated")
 
 if __name__ == "__main__":
     main()
