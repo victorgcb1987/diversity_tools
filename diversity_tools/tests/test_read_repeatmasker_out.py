485193621002be5a08a5875e039888b097df35bd
AgustinAmata <137272008+AgustinAmata@users.noreply.github.com>
2023-10-11 13:32:53 +0200

    Update test_read_repeatmasker_out.py

--- diversity_tools/tests/test_read_repeatmasker_out.py
+++ diversity_tools/tests/test_read_repeatmasker_out.py
@@ -1,6 +1,9 @@
 import unittest
 from pathlib import Path
 
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
 from src.matrix_readers import read_repeatmasker_out
 
 class RepMask(unittest.TestCase):
@@ -13,14 +16,30 @@ class RepMask(unittest.TestCase):
     def test_read_repmask(self):
         with open(self.test_path) as input_fhand:
             read_repeats = read_repeatmasker_out(input_fhand)
-        assert read_repeats[0] == {
-            "sw": "25785", "per div": "14.7", "per del": "2.1",
-            "per ins": "2.4", "seqid": "Peame105C00", "start": "23",
-            "end": "4959", "q left": "55849342", "match": "+",
-            "repeat": "rnd-4_family-1935", "class": "Unknown",
-            "superfamily": "Unknown", "r start": "1", "r end": "4924",
-            "r left": "0", "id": "1", "length": 4936
-            }
+        convert_dict = {
+        "per div": "float16", "per del": "float16",
+        "per ins": "float16","start": "int32", "end": "int32",
+        "repeat": "category", "length": "int32", "class": "category",
+        "superfamily": "category"
+        }
+        test_df = pd.DataFrame([{
+            "per div": "14.7", "per del": "2.1", "per ins": "2.4",
+            "seqid": "Peame105C00", "start": "23", "end": "4959",
+            "repeat": "rnd-4_family-1935", "length": 4936, "class": "Unknown",
+            "superfamily": "Unknown"
+            },{
+            "per div": "31.9", "per del": "5.0", "per ins": "5.0",
+            "seqid": "Peame105C00", "start": "4959", "end": "5519",
+            "repeat": "ltr-1_family-724", "length": 560, "class": "LTR",
+            "superfamily": "Copia"
+            },{
+            "per div": "32.2", "per del": "4.6", "per ins": "4.6",
+            "seqid": "Peame105C00", "start": "5630", "end": "5847",
+            "repeat": "ltr-1_family-645",  "length": 217, "class": "LTR",
+            "superfamily": "Copia"
+            }]).astype(convert_dict)
+        
+        assert_frame_equal(test_df, read_repeats)
         
 if __name__ == "__main__":
     unittest.main()
